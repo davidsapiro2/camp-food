b525a66536451ccfb2c602f9b0319263d89de5f2
David Sapiro <dsap48@gmail.com>
2024-09-23 12:36:10 -0700

    added basic post route along with schemas.py with body validation schemas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from schemas import Item
 
 app = FastAPI()
 
@@ -15,3 +16,8 @@ def read_item(item_id: int): #optional typing included, this validates and/or co
 @app.get("/items") #localhost:3000/?name=david
 def read_item_query(name: str = 'John', num: int | None = None): #optional typing and default included, will validate and/or convert type
     return {"name": name}
+
+### request body ###
+@app.post("/items")
+def create_item(item: Item):
+    return item
