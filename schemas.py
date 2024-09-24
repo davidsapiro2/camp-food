42d1a708e7e96b3e77ec8f7e20f8fd005858928e
David Sapiro <dsap48@gmail.com>
2024-09-23 17:29:18 -0700

    added users schema and updated users and items to work with database models

--- schemas.py
+++ schemas.py
@@ -2,10 +2,12 @@
 
 from pydantic import BaseModel
 
-class Item(BaseModel):
-    name: str
+### Item models ###
+
+class ItemBase(BaseModel):
+    title: str
     description: str
-    photo: str | None = None
+    photo_url: str | None = None
 
     model_config = {
         "extra": "forbid", # forbids other fields than those declared
@@ -19,3 +21,40 @@ class Item(BaseModel):
             ]
         }
     }
+
+class Item(ItemBase):
+    id: int
+    user_id: int
+
+    class Config:
+        orm_mode = True
+
+### User Models ###
+
+class UserBase(BaseModel):
+    name: str
+    username: str
+    email: str
+
+    model_config = {
+        "extra": "forbid", # forbids other fields than those declared
+        "json_schema_extra": { # example request body for the docs
+            "examples" : [
+                {
+                    "name": "David",
+                    "username": "foodDave",
+                    "email": "food@dave.com"
+                }
+            ]
+        }
+    }
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    items: list[Item] = []
+
+    class Config:
+        orm_mode = True
