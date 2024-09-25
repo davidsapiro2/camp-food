734ac88dfa3679ff35029f9079ef16464c7b7b37
David Sapiro <dsap48@gmail.com>
2024-09-24 18:00:46 -0700

    updated database name to actual

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql:///camp-food"
+SQLALCHEMY_DATABASE_URL = "postgresql:///campfood"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
