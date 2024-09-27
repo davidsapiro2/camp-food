b037a57ba900e0a1326c2e94e90d341d0bd95560
David Sapiro <dsap48@gmail.com>
2024-09-27 16:04:03 -0700

    refactored util functions to be more specific to a model/db table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from .schemas import Item
 from .database import SessionLocal, engine
 from . import models, schemas
 from .utils import Crud
@@ -17,28 +16,19 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
 @app.get("/items/{item_id}", response_model=schemas.Item)
 def read_item(item_id: int, db: Session = Depends(get_db)): #optional typing included, this validates and/or convert type
 
-    item = Crud.get_one(db=db, model=models.Item, id=item_id)
+    item = Crud.get_item_by_id(db=db, id=item_id)
     return item
 
 @app.get("/items", response_model=list[schemas.Item])
 def read_all_items(db: Session = Depends(get_db)):
 
-    items = Crud.get_all(db=db, model=models.Item)
+    items = Crud.get_items(db=db)
     return items
-
-### example query parameter ###
-@app.get("/items") #localhost:3000/?name=david
-def read_item_query(name: str = 'John', num: int | None = None): #optional typing and default included, will validate and/or convert type
-    return {"name": name}
 
 @app.post("/items", response_model=schemas.Item)
 def create_item(item: schemas.Item, db: Session = Depends(get_db)):
-    db_item = Crud.create_one(db=db, model=models.Item, data=item, user_id=1)
+    db_item = Crud.create_item(db=db, data=item, user_id=1)
     return db_item
--- utils.py
+++ utils.py
@@ -4,22 +4,23 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 class Crud:
-    def get_one(db: Session, model: models, id: int):
+    def get_item_by_id(db: Session, id: int):
         '''Gets and returns one data item from database for the passed model and id'''
 
-        data = db.query(model).filter(model.id == id).first()
+        data = db.query(models.Item).filter(models.Item.id == id).first()
         return data
 
-    def get_all(db: Session, model: models):
+    def get_items(db: Session):
         '''Gets and returns all data from database for the passed model'''
 
-        data = db.query(model).all()
+        data = db.query(models.Item).all()
         return data
 
-    def create_one(db: Session, model: models, data: schemas, user_id: int):
+    def create_item(db: Session, data: schemas.Item, user_id: int):
         '''Creates a database entry from passed data'''
 
-        item = model(**data.dict())
+        item = models.Item(**data.model_dump())
+        item.user_id = user_id
         db.add(item)
         db.commit()
         db.refresh(item)
